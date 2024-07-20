391c172770199c03cbb21d185665f0075dd231b4
kmadison382 <k.madison.382@gmail.com>
2024-07-20 16:25:07 -0500

    wrote the main program to add animals to zoo

--- FactoryPattern/Program.cs
+++ FactoryPattern/Program.cs
@@ -4,7 +4,20 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var outShopping = true;
+            while (outShopping == true)
+            {
+                Console.WriteLine("What should we add to the zoo?");
+                string newAnimal = Console.ReadLine();
+                IZoo animal = ZooFactory.GetAnimal(newAnimal);
+                animal.Get();
+                Console.WriteLine("Would you like to add another animal?");
+                string keepAdding = Console.ReadLine();
+                if (keepAdding.ToLower() == "no")
+                {
+                    outShopping = false;
+                }
+            }
         }
     }
 }
